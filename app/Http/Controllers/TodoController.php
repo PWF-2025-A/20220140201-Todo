ab66193f4be25f4a78a1fb8ade4af1c058746f0d
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-04-25 22:25:21 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,10 +9,21 @@ use Illuminate\Support\Facades\Auth;
 class TodoController extends Controller
 {
     public function index() {
-        $todos = Todo::all();
-        //$todos = Todo::where('user_id', Auth::id())->get();
-        dd($todos);
-        return view("todo.index");
+        //$todos = Todo::all();
+        $todos = Todo::where('user_id', Auth::id())->orderBy('created_at', 'desc')->get();
+        // dd($todos);
+        return view("todo.index", compact('todos'));
+    }
+    public function store(Request $request){
+    $request->validate([
+        'title' => 'required|string|max:255',
+    ]);
+
+    $todo = Todo::create([
+        'title' => ucfirst($request->title),
+        'user_id' => Auth::id(),
+    ]);
+    return redirect()->route('todo.index')->with('success', 'Todo created successfully.');
     }
     public function create() {
         return view("todo.create");
